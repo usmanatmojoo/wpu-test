8853ffa3c7c42dbbb8137098592f783fca0ad86a
dell <usmanatmojoo@gmail.com>
2021-05-27 19:50:56 +0700

    menambahkan file index.php

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Coba git Remote</title>
 </head>
 <body>
-  <p>paragrafh<p>  
+<h1>Hello World!<h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio consequuntur non molestias vitae minima sed explicabo, temporibus magnam accusamus debitis placeat quam, deleniti sit hic fugit labore nulla libero? Mollitia.<p>  
 </body>
 </html>
